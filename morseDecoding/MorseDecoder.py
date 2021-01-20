85d971f6a34ae16e6cc7e55f5055f082416dee81
bmksimons <69849576+bmksimons@users.noreply.github.com>
2021-01-20 11:44:36 +0100

    Update MorseDecoder.py

--- morseDecoding/MorseDecoder.py
+++ morseDecoding/MorseDecoder.py
@@ -19,10 +19,6 @@ class MorseDecoder():
         self.T = 0.5
         self.result = ""
         GPIO.setup(PIN_BTN, GPIO.IN, GPIO.PUD_UP)
-        GPIO.setup(PIN_RED_LED_0, GPIO.OUT, GPIO.LOW)
-        GPIO.setup(PIN_RED_LED_1, GPIO.OUT, GPIO.LOW)
-        GPIO.setup(PIN_RED_LED_2, GPIO.OUT, GPIO.LOW)
-        GPIO.setup(PIN_BLUE_LED, GPIO.OUT, GPIO.LOW)
 
     def decoding_loop(self):
         """ the main decoding loop """
@@ -58,12 +54,8 @@ class MorseDecoder():
         """ handle the signals using corresponding functions """
         if self.evaluate_input() == 0:  # was the signal a dot/dash?
             if signal > self.T:
-                GPIO.output(PIN_RED_LED_0, GPIO.HIGH)
-                GPIO.output(PIN_RED_LED_1, GPIO.HIGH)
-                GPIO.output(PIN_RED_LED_2, GPIO.HIGH)
                 self.update_current_symbol("-")
             else:
-                GPIO.output(PIN_BLUE_LED, GPIO.HIGH)
                 self.update_current_symbol(".")
         elif self.evaluate_input() == 1:  # was the signal a pause?
             if signal > 7 * self.T:  # is the signal the end of a word?
@@ -83,13 +75,6 @@ class MorseDecoder():
         """ append the signal to current symbol code """
         self.current_symbol += signal
         self.set_leds_low()
-
-    def set_leds_low(self):
-        """ resets the LED outputs after a signal has been registered """
-        GPIO.output(PIN_RED_LED_0, GPIO.LOW)
-        GPIO.output(PIN_RED_LED_1, GPIO.LOW)
-        GPIO.output(PIN_RED_LED_2, GPIO.LOW)
-        GPIO.output(PIN_BLUE_LED, GPIO.LOW)
 
     def update_current_word(self, symbol):
         """ append the symbol to the current word """
